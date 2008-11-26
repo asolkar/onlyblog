c4abe1533a4107a6164013d74208e0d179daf2ba
Mahesh Asolkar <mahesh@mahesha.com>
2008-11-26 07:41:39 +0000

    Initial separation of presentation to theme area

--- index.php
+++ index.php
@@ -6,34 +6,9 @@
 include 'lib/config.inc';
 include 'config.inc';
 include 'lib/onlyblog.inc';
+include $__config['theme_dir'] . "/theme.php";
 
 blog_init();
+
+index_page();
 ?>
-<?php
-
-echo '<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'
-?>
-
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-  <title><?php echo $__status['page_title'] ?></title>
-  </title>
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
-  <link rel="StyleSheet" href="<?php echo $__config['css_file'] ?>" type="text/css" title="Design Style">
-
-  <script type="text/javascript" language="javascript" src="Library.js" />
-  <script type="text/javascript">
-  //<![CDATA[
-  //]]>
-  </script>
-</head>
-<body>
-<h1><?php echo $__config['blog_name'] ?></h1>
-<?php
- get_post_list()
-?>
-</body>
-</html>
---
+++ serene/theme.php
@@ -0,0 +1,62 @@
+<?php
+
+function http_doc_type() {
+  echo '<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+}
+
+function page_header () {
+  global $__status, $__config;
+
+  echo <<<END
+    <div id="page_header">
+      <dif id="blog_name"><h1>{$__config['blog_name']}</h1></dif>
+      <dif id="blog_tag_line"><h3>{$__config['blog_tag_line']}</h3></dif>
+    </div>
+END;
+}
+function page_footer () {
+  global $__status, $__config;
+
+  echo <<<END
+    <div id="page_footer">
+      {$__config['blog_name']} is an OnlyBlog blog using the Serene theme.
+    </div>
+END;
+}
+
+function index_page () {
+  global $__status, $__config;
+
+  echo <<<END
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <title>{$__status['page_title']}</title>
+  </title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+  <link rel="StyleSheet" href="{$__config['theme_dir']}/{$__config['css_file']}"
+        type="text/css" title="Serene Design Style">
+
+  <script type="text/javascript" language="javascript" src="Library.js" />
+  <script type="text/javascript">
+  //<![CDATA[
+  //]]>
+  </script>
+</head>
+<body>
+END;
+
+  page_header();
+
+  get_post_list();
+
+  page_footer();
+
+  echo <<<END
+</body>
+</html>
+END;
+}
+?>
